1c0e58f55235cd1c15b1fd20dfbb297b57324438
Nuri <nuriddin7612@mail.ru>
2022-04-25 21:48:10 +0900

    Flash Chat App

---
+++ android/app/src/main/kotlin/com/example/project_chat_flash/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_chat_flash
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:project_chat_flash/constants.dart';
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  static const String id = 'chat_screen';
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
